947b6cdd3a67b3b3e8459ac82a722cf68de15b7e
daniel arega <danielarega25@gmail.com>
2025-07-08 11:03:35 +0300

    i added the dark mode for header section

--- myportfolio/app/page.tsx
+++ myportfolio/app/page.tsx
@@ -11,6 +11,14 @@ import Footer from './components/Footer';
 export default function Home() {
 
   const [isDarkMode, setIsDarkMode]=useState(true)
+
+  useEffect(()=>{
+if (localStorage.theme==='dark' ||(!('theme' in localStorage) && window.matchMedia('(prefers-color-scheme:dark)').matches)) {
+  setIsDarkMode(true)
+} else {
+  setIsDarkMode(false)
+}
+  },[])
   useEffect(()=>{
 if(isDarkMode){
   document.documentElement.classList.add('dark');
