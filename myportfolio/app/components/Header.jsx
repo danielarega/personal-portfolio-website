4f3f94f63840a2495825e150f1596fe12822aea7
daniel arega <danielarega25@gmail.com>
2025-07-08 11:09:29 +0300

    i added the dark mode to header by fixing the css

--- myportfolio/app/components/Header.jsx
+++ myportfolio/app/components/Header.jsx
@@ -18,9 +18,9 @@ const Header = () => {
               <p className='max-w-2xl mx-auto font-ovo'>I am a junior frontend developer from ethiopia with modern stack technologies to build responsive and user-friendly web application </p>
 
  <div className='flex flex-col sm:flex-row items-center gap-4 mt-4'>
-<a href="#contact" className='px-10 py-3 border border-white  rounded-full bg-black text-white  flex items-center gap-2'>contact me <Image src={assets.right_arrow_white} alt="" className=' w-4' /></a>
+<a href="#contact" className='px-10 py-3 border border-white  rounded-full bg-black text-white  flex items-center gap-2 dark:bg-transparent'>contact me <Image src={assets.right_arrow_white} alt="" className=' w-4' /></a>
 
-<a href="/myresume.pdf" download className='px-10 py-3 border rounded-full border-gray-500 flex items-center gap-2'>my resume <Image src={assets.download_icon} alt="" className=' w-4' /></a>
+<a href="/myresume.pdf" download className='px-10 py-3 border rounded-full border-gray-500 flex items-center gap-2 bg-white dark:text-black'>my resume <Image src={assets.download_icon} alt="" className=' w-4' /></a>
 
               </div>
     </div>
