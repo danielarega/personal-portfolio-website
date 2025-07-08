5d6dc6d332666f493b333d78bffec895f8efe9f3
daniel arega <danielarega25@gmail.com>
2025-07-08 11:25:30 +0300

    i added the darkmode on my service pages

--- myportfolio/app/components/Services.jsx
+++ myportfolio/app/components/Services.jsx
@@ -10,10 +10,10 @@ export default function Services() {
       <p className='text-center max-w-2xl mx-auto mt-5 mb-12   font-ovo'>i am frontend developer from ethiopia with zero year of experience in multiple companies like arsi university, gebeya.com.</p>
       <div className='grid grid-cols-auto gap-6 my-10'>
 {serviceData.map(({icon,title,description,link} , index)=>(
-  <div key={index} className='border border-gray-400 rounded-lg px-8 py-12 hover:shadow-black cursor-pointer hover:bg-lightHover hover:-translate-y-1 duration-500'>
+  <div key={index} className='border border-gray-400 rounded-lg px-8 py-12 hover:shadow-black cursor-pointer hover:bg-lightHover hover:-translate-y-1 duration-500 dark:hover:bg-darkHover dark:hover:shadow-white'>
 <Image src={icon} alt='icon' className='w-10'/>
-<h3 className='text-lg my-4 text-gray-700'>{title}</h3>
-<p className='text-sm text-gray-600 leading-5'>
+<h3 className='text-lg my-4 text-gray-700 dark;text-white'>{title}</h3>
+<p className='text-sm text-gray-600 leading-5 dark;text-white/80'>
   {description}
 </p>
 <a href={link} className='flex items-center gap-2 text-sm  mt-5'>Read more <Image src={assets.right_arrow} alt='right arrow' className='w-4'/></a>
