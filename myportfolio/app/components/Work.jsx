ebee1385416c4b9a1eccd7143a3432608aa1da29
daniel arega <danielarega25@gmail.com>
2025-07-08 11:33:38 +0300

    i added some style to my work section when the dark mode is on

--- myportfolio/app/components/Work.jsx
+++ myportfolio/app/components/Work.jsx
@@ -1,7 +1,7 @@
 import Image from 'next/image'
 import React from 'react'
 import { assets, workData } from '../../assets/assets.js' 
-const Work = () => {
+const Work = (isDarkMode) => {
   return (
     <div id='work' className='w-full px-[12%] py-10 scroll-mt-20'>
       <h4 className='text-center mb-2 text-lg font-ovo'>my portfolio</h4>
@@ -9,7 +9,7 @@ const Work = () => {
 
       <p className='text-center max-w-2xl mx-auto mt-5 mb-12   font-ovo'>welcome to my web development portfolio! Explore the collection of projects showcasing my expertise in the frontend development.</p>
 
-      <div className='grid grid-cols-auto ny-10 gap-5'>
+      <div className='grid grid-cols-auto ny-10 gap-5 dark:text-black'>
         {workData.map((project, index) => (
             <div key={index}  className='aspect-square bg-no-repeat bg-cover bg-center rounded-lg relative cursor-pointer group'    style={{backgroundImage:`url(${project.bgImage})`}}>
                <div className='bg-white w-10/12 rounded-md absolute bottom-5 left-1/2 -translate-x-1/2 py-3 px-5 flex items-center justify-between duration-500 group-hover:bottom-7'>
@@ -25,7 +25,7 @@ const Work = () => {
         ))}
       </div>
 
-      <a href="" className='w-max flex items-center  justify-center gap-2 text-gray-700 border-[0.5px] border-gray-700 rounded-full py-3 px-10 mx-auto my-20 hover:bg-lightHover duration-500'>show more <Image src={assets.right_arrow_bold} alt='Right arrow' className='w-4'/> </a>
+      <a href="" className='w-max flex items-center  justify-center gap-2 text-gray-700 border-[0.5px] border-gray-700 rounded-full py-3 px-10 mx-auto my-20 hover:bg-lightHover duration-500 darktext-white dark:border-white dark:hover:bg_darkHover'>show more <Image src={isDarkMode ? assets.right_arrow_bold_dark: assets.right_arrow_bold} alt='Right arrow' className='w-4'/> </a>
     </div>
 
 
